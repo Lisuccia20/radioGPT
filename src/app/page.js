f69306d1f3b63ef3d62a14980c3b3ee6a26513fa
Lisuccia20 <l.manoni2@gmail.com>
2025-03-13 09:33:54 +0100

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,174 @@
+'use client'
+import {useEffect, useRef, useState} from 'react';
+import styles from "./page.module.css";
+import OpenAI from 'openai';
+import axios from 'axios';
+
+export default function Home() {
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
+    const [playlist, setPlaylist] = useState('');
+    const [fileName, setFileName] = useState('');
+    const [duration, setDuration] = useState(0);
+    const [playState, setPlayState] = useState(0)
+    const [isFirstPlay, setIsFirstPlay] = useState(true);
+    const [startTime, setStartTime] = useState(0)
+
+    // Ref to store the audio object
+    const audioRef = useRef(null);
+    const gptRef = useRef(null);
+
+
+
+    const play = async () => {
+        try {
+            audioRef.current = new Audio();
+            gptRef.current = new Audio();
+            setPlayState(playState + 1);
+            const metadataSong = await axios.get(`/api/playlists?query=${playlist}`);
+            console.log(metadataSong.data.filename);
+            setFileName(metadataSong.data.filename);
+            let id = metadataSong.data.id;
+            setDuration(metadataSong.data.duration);
+            await Stream(id);
+        } catch (error) {
+            console.log('Error fetching song data:', error);
+        }
+    };
+
+    useEffect(() => {
+        if (duration === 0) return;
+        const gptResponse = async () => {
+            try {
+                const response = await openai.chat.completions.create({
+                    model: "gpt-4o-mini-audio-preview",
+                    modalities: ["text", "audio"],
+                    audio: {voice: "onyx", format: "mp3"},
+                    messages: [
+                        {
+                            role: "system",
+                            content: "you are an italian dj from the station radiogpt, you should talk about the song you just played in the text delimited by the triple apostrophe (remove topic from the artist name if there is any), tell the news from time to time, only speak in italian"
+                        },
+                        {
+                            role: "user",
+                            content: `""" in: ${fileName}"""`
+                        }
+                    ],
+                    store: true,
+                });
+
+                gptRef.current.src = `data:audio/mp3;base64,${response.choices[0].message.audio.data}`
+
+            } catch (error) {
+                console.log('Error generating GPT response:', error);
+            }
+        }
+
+        gptResponse()
+    }, [fileName, duration]);
+
+    useEffect(() => {
+        if(startTime === 0) return;
+        if(gptRef.current)  {
+            gptRef.current.addEventListener('loadedmetadata', () => {
+                const durationGpt = gptRef.current.duration;
+                console.log('GPT Audio Duration:', durationGpt);
+                const requestTime = Date.now() - startTime;
+                console.log('requestTime:', requestTime);
+                const delay = duration - durationGpt / 2;
+                console.log('GPT Audio Delay:', delay);
+                setTimeout(() => {
+                    fadeAudio(audioRef.current, 0.2, 1, gptRef.current);
+                    gptRef.current.addEventListener('ended', () => {
+                        fadeAudio(audioRef.current, 1, 1);
+                    });
+                }, (delay * 1000) - requestTime);
+
+            });
+        }
+    }, [startTime]);
+
+    const Stream = async (id) => {
+        console.log(`Start stream for ID: ${id}`);
+        try {
+
+            if (audioRef.current) {
+                audioRef.current.pause(); // Ensure any previous audio is stopped
+                audioRef.current.src = ''; // Reset the source
+            }
+
+            audioRef.current.src = `/api/stream?id=${id}`
+
+            if (isFirstPlay) {
+                audioRef.current.volume = 1; // Full volume for the first playback
+                setIsFirstPlay(false); // Update the flag after the first playback
+            } else {
+                audioRef.current.volume = 0.2; // Lower volume for subsequent playbacks
+            }
+            if (audioRef.current) {
+                audioRef.current.pause(); // Ensure any previous audio is stopped
+                audioRef.current.src = ''; // Reset the source
+            }
+
+            const loadTimeout = setTimeout(() => {
+                if (audioRef.current != null){
+                    console.log('Audio took too long to load, retrying...');
+                    play();
+                }
+
+            }, 5000);
+
+            audioRef.current.addEventListener('canplaythrough', () => {
+                audioRef.current.play();
+                clearTimeout(loadTimeout);
+                setStartTime(Date.now());
+                console.log(`start time: ${startTime}`)
+                console.log('Audio can play through.');
+            });
+
+            audioRef.current.addEventListener('ended', () => {
+                play();
+            });
+
+        } catch (error) {
+            console.log('Error initializing audio stream:', error);
+        }
+    };
+
+    // Fade-in or fade-out effect function
+    const fadeAudio = (audio, targetVolume, durationInSeconds, audioToPlay = null) => {
+        if (!audio || targetVolume < 0 || targetVolume > 1) return; // Safety checks
+
+        const startVolume = audio.volume;
+        const volumeChangePerStep = (targetVolume - startVolume) / (durationInSeconds * 10); // Adjust volume every 100ms
+
+        let intervalId = setInterval(() => {
+            if (Math.abs(audio.volume - targetVolume) < 0.01) {
+                clearInterval(intervalId);
+                setSongVolume(targetVolume);
+                if(audioToPlay){
+                    audioToPlay.play();
+                    audioToPlay.volume = 1;
+                }
+            } else {
+                audio.volume += volumeChangePerStep;
+                // Ensure volume stays within bounds
+                if (audio.volume < 0) audio.volume = 0;
+                if (audio.volume > 1) audio.volume = 1;
+            }
+        }, 100); // Adjust volume every 100 milliseconds
+    };
+
+    return (
+        <div className={styles.page}>
+            <h1>RadioGPT</h1>
+            <h2>{fileName}</h2>
+            <input
+                type="text"
+                value={playlist}
+                onChange={(e) => setPlaylist(e.target.value)}
+            />
+            <button onClick={() => play()}>Play</button>
+        </div>
+    );
+}
---
+++ src/pages/api/playlists.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import ytdl from '@distube/ytdl-core';
+import { PassThrough } from 'stream';
+import { Buffer } from 'buffer';
+import { parseBuffer } from 'music-metadata';
+
+
+
+
+function iso8601ToSeconds(isoDuration) {
+    const regex = /P(?:([\d.]+)Y)?(?:([\d.]+)M)?(?:([\d.]+)D)?(?:T(?:([\d.]+)H)?(?:([\d.]+)M)?(?:([\d.]+)S)?)?/;
+    const matches = regex.exec(isoDuration);
+
+    const years = parseFloat(matches[1] || 0);
+    const months = parseFloat(matches[2] || 0);
+    const days = parseFloat(matches[3] || 0);
+    const hours = parseFloat(matches[4] || 0);
+    const minutes = parseFloat(matches[5] || 0);
+    const seconds = parseFloat(matches[6] || 0);
+
+    // Convert everything to seconds
+    const totalSeconds =
+        seconds +
+        minutes * 60 +
+        hours * 3600 +
+        days * 86400 +
+        months * 2628000 + // approx. 30.44 days per month
+        years * 31536000;  // approx. 365.25 days per year
+
+    return totalSeconds;
+}
+
+
+
+export default async function handler(req, res) {
+    const { query } = req.query;
+    const apiKey = process.env.NEXT_PUBLIC_YT_API_KEY;
+
+    if (!query) {
+        return res.status(400).json({ error: 'Query parameter is required' });
+    }
+
+    const url = new URL(query);
+    const playlistId = url.searchParams.get("list");
+
+    // Fetch playlist songs
+    const playlistSongs = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+        params: {
+            key: apiKey,
+            part: 'snippet',
+            playlistId: playlistId,
+            maxResults: 100,
+        }
+    });
+
+    const songsIds = [];
+    const songsNames = [];
+    playlistSongs.data.items.forEach((item) => {
+        songsIds.push(item.snippet.resourceId.videoId);
+        songsNames.push(`${item.snippet.title} - ${item.snippet.videoOwnerChannelTitle}`);
+    });
+
+    const randomIndex = Math.floor(Math.random() * songsIds.length);
+    const randomSongId = songsIds[randomIndex];
+
+    const duration = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+        params: {
+            key: apiKey,
+            part: 'contentDetails',
+            id: randomSongId,
+        }
+    })
+
+
+    res.status(200).json({
+        id: randomSongId,
+        filename: songsNames[randomIndex],
+        duration: iso8601ToSeconds(duration.data.items[0].contentDetails.duration),
+    })
+}
+
