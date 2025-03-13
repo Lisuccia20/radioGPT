f967be75c70f978471a3c422b66580a0a747c5e9
Lisuccia20 <l.manoni2@gmail.com>
2025-03-13 09:57:04 +0100

    https

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const { id } = req.query;
 
     try{
-        const audio = ytdl(`http://www.youtube.com/watch?v=${id}`, {
+        const audio = ytdl(`https://www.youtube.com/watch?v=${id}`, {
             quality: 'highestaudio',
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
