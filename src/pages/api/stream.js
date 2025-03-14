cb4813dbec20311ea2bc371c7a3925db494293c7
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 21:39:30 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -1,16 +1,9 @@
-import ytdl from '@distube/ytdl-core'
-import {HttpsProxyAgent} from 'https-proxy-agent';
-import * as fs from "node:fs";
+import ytdl from '@distube/ytdl-core';
 export default async function handler(req, res) {
     const { id } = req.query;
-    const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json", 'utf8')));
     try {
-
-
-        ytdl(`https://www.youtube.com/watch?v=${id}`, {
-            agent: agent
-        }).pipe(res);
-
+        const url = `https://www.youtube.com/watch?v=${id}`;
+        ytdl(url).pipe(res);
     } catch (e) {
         console.error("Error streaming the video:", e);
         res.status(500).json({ error: e.message || 'Internal server error' });
