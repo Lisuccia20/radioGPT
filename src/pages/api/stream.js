232785f9b2f2432a119d2fe59582a622ae8ae605
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 11:02:12 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -3,14 +3,14 @@ import path from 'path';
 
 export default async function handler(req, res) {
     const { id } = req.query;
-    const cookiesPath = path.resolve(__dirname, 'cookies.firefox-private.txt');
+    const cookiesPath = 'chrome';
     try{
         const ytDlp = new YTDlpWrap('ytp-dlp-stream/binary');
         let readableStream = ytDlp.execStream([
             `https://www.youtube.com/watch?v=${id}`,
             '-f',
             'best[ext=mp4]',
-            `--cookies=${cookiesPath}`
+            `--cookies-from-browser=${cookiesPath}`
         ]);
         readableStream.pipe(res);
     }catch(e){
