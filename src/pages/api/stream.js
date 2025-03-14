2f82ebf488aa397e109439e3ca220e008b3f8d98
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 16:51:55 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -1,37 +1,10 @@
-import ytstream from 'yt-stream';
+import ytdl from '@distube/ytdl-core'
 
 export default async function handler(req, res) {
     const { id } = req.query;
-    const apiKey = process.env.NEXT_PUBLIC_YT_API_KEY;
-    ytstream.setApiKey(apiKey);
-    ytstream.setPreference('api', 'IOS');
-
-    const agent = new ytstream.YTStreamAgent([], {
-        keepAlive: true,
-        keepAliveMsecs: 5000
-    });
-
-    agent.syncFile('./cookies.json');
-    ytstream.setGlobalAgent(agent);
 
     try {
-        const stream = await ytstream.stream(`https://youtu.be/watch?v=${id}`, {
-            quality: 'high',
-            type: 'audio+video',
-        });
-
-        // Set the correct headers for audio streaming
-        res.setHeader('Content-Type', 'audio/mpeg');
-        res.setHeader('Content-Disposition', 'inline');
-
-        // Handle stream errors
-        stream.stream.on('error', (err) => {
-            console.error("Stream error:", err);
-            res.status(500).json({ error: "Stream failed" });
-        });
-
-        // Pipe the stream to the response
-        stream.stream.pipe(res);
+        ytdl(`https://www.youtube.com/watch?v=${id}`).pipe(res);
 
     } catch (e) {
         console.error("Error streaming the video:", e);
