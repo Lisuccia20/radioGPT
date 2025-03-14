fdd8c483f670c51ed9c5b277a91112bde610e345
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 10:52:04 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default async function handler(req, res) {
     const { id } = req.query;
-    const cookiesPath = path.resolve(__dirname, 'cookies.firefox-private.txt');
+    const cookiesPath = path.resolve(__dirname, 'cookies.txt');
     try{
         const ytDlp = new YTDlpWrap('ytp-dlp-stream/binary');
         let readableStream = ytDlp.execStream([
