ee98d6f609e3c282b82b11aaa39b2e167a5d87d0
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 17:27:21 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -3,9 +3,22 @@ import fs from 'fs';
 export default async function handler(req, res) {
     const { id } = req.query;
 
+    const options = {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+        dl_priority: 1,
+        highWaterMark: 1 << 25, // 32MB
+        // You can add custom headers to simulate a legitimate user (optional)
+        requestOptions: {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+        }
+    };
+
     try {
         const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
-        ytdl(`https://www.youtube.com/watch?v=${id}`,{agent}).pipe(res);
+        ytdl(`https://www.youtube.com/watch?v=${id}`,options).pipe(res);
 
     } catch (e) {
         console.error("Error streaming the video:", e);
