89c4bdc5eea90b584d2dc9a1f090e98653f6ddaf
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 20:18:28 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -2,9 +2,11 @@ import ytdl from '@distube/ytdl-core'
 import {HttpsProxyAgent} from 'https-proxy-agent';
 export default async function handler(req, res) {
     const { id } = req.query;
-
+    const agent = ytdl.createProxyAgent({ uri: 'http://122.200.19.103'});
     try {
-        ytdl(`https://www.youtube.com/watch?v=${id}`).pipe(res);
+
+
+        ytdl(`https://www.youtube.com/watch?v=${id}`, { agent: agent }).pipe(res);
 
     } catch (e) {
         console.error("Error streaming the video:", e);
