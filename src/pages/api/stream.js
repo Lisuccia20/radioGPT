480d7744d60e6f20f21f6f0b5f5e7852231f7c0f
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 14:52:07 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const { id } = req.query;
     const apiKey = process.env.NEXT_PUBLIC_YT_API_KEY;
     ytstream.setApiKey(apiKey);
-    ytstream.setPreference('api', 'ANDROID');
+    ytstream.setPreference('scrape', 'ANDROID');
     try{
         const stream = await ytstream.stream(`https://www.youtube.com/watch?v=${id}`, {
             quality: 'high',
