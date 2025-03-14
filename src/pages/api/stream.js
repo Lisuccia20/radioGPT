ff50309c30ed73ec73559d73e12a35ce82793c7f
Lisuccia20 <l.manoni2@gmail.com>
2025-03-14 12:01:11 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
             `https://www.youtube.com/watch?v=${id}`,
             '-f',
             'best[ext=mp4]',
-            `--cookies=${cookiesPath}`
+            `--cookies=AFmmF2swRQIhAOGfUjIVqkZO_WbushB8M1GwZEh686fD2eTaXzhLAaZDAiAwm8JxD2kWCeECFTlou0YQY6iaOHMzNj6O8gTCPnKmow:QUQ3MjNmeHkxMzBnSmdPZGZXRXNnOEhSRWFTM2Q2M3ZXR1RaYllOZm0xSUcwOHVId1kyejg4OFRxZ3BfQ3RYaU4tQUhQSmpMSmVNd3QwTWxkdkZwU1BjN2gzblNEN295Uzl4MUZUZFZpVjZCME9JWk1EdFIzYm1PNEpwYlRVRDUtcDRnYVZwc1RKWW9ySjh0SXhtNXpCMEY2d3VKbFBETkFB`
         ]);
         readableStream.pipe(res);
     }catch(e){
