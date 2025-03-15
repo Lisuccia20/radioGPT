f89bf225ff5119bddaeba06c999e424ac8142645
Lisuccia20 <l.manoni2@gmail.com>
2025-03-15 10:08:31 +0100

    revised logic

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -1,14 +1,15 @@
-import { YtdlCore, toPipeableStream } from '@ybd-project/ytdl-core';
+import youtubedl from 'youtube-dl-exec'
 export default async function handler(req, res) {
     const { id } = req.query;
     try {
-        const ytdl = new YtdlCore({
-            hl: 'en',
-            gl: 'US',
-            streamType: 'nodejs',
-        });
         const url = `https://www.youtube.com/watch?v=${id}`;
-        ytdl.download(url, { filter: "audioonly"}).then((stream) => toPipeableStream(stream).pipe(res));
+        const process = await youtubedl(url, {
+            format: 'bestaudio/best',
+            output: '-', // Output to stdout
+            extractAudio: true,
+            audioFormat: 'mp3',
+        }, { stdio: ['ignore', 'pipe', 'pipe'] });
+        process.pipe(res);
     } catch (e) {
         console.error("Error streaming the video:", e);
         res.status(500).json({ error: e.message || 'Internal server error' });
